751540a810516b77b4c92e25906b94d8b8de7d74
Ryan Xavier Leska <ryan@leskafamily.com>
2019-12-09 08:15:05 -0600

    changes .json to .webmanifest

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -15,7 +15,7 @@ let snowflakes = [];
 var counter = 0;
 var snowFreq;
 var sign;
-var version = "Version 1.9.1";
+var version = "Version 1.9.2";
 //Setup: Canvas, background, init objects
 function setup() {
   //Canvas and Background
