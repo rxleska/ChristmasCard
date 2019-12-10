a8544b0bcaef1d423a3d57b2060366069ef43b34
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-10 08:03:22 -0600

    Checking install prompt version change

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -15,7 +15,7 @@ let snowflakes = [];
 var counter = 0;
 var snowFreq;
 var sign;
-var version = "Version 2.0.5";
+var version = "Version 2.0.6";
 //Setup: Canvas, background, init objects
 function setup() {
   //Canvas and Background
