f2341a203ca9559f81f3c7f9df92a87052033817
Ryan Xavier Leska <ryan@leskafamily.com>
2019-12-11 08:10:05 -0600

    added logo size and change location

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -15,7 +15,7 @@ let snowflakes = [];
 var counter = 0;
 var snowFreq;
 var sign;
-var version = "Version 2.0.6";
+var version = "Version 2.0.7";
 //Setup: Canvas, background, init objects
 function setup() {
   //Canvas and Background
