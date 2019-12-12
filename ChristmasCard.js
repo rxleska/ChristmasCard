994327d35ffda789e9c238d5240a888a6f14ba47
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-11 18:50:48 -0600

    added a 512 logo alongside the 192

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -15,7 +15,7 @@ let snowflakes = [];
 var counter = 0;
 var snowFreq;
 var sign;
-var version = "Version 2.0.13";
+var version = "Version 2.0.14";
 
 //Setup: Canvas, background, init objects
 function setup() {
