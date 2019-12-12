f5ea988e2c67ac55cc11c93cf2364058af69a3e3
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-12 17:43:53 -0600

    added apple icon to manifest

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -17,7 +17,7 @@ var counter = 0;
 var snowFreq;
 var sign;
 var saveCo = 99;
-var version = "Version 2.2.2";
+var version = "Version 2.2.3";
 
 //Setup: Canvas, background, init objects
 function setup() {
