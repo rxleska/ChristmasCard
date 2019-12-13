d023c211a41575ce32b521d62d07a24d35566daf
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-13 16:08:50 -0600

    added id to link for apple icon

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -17,7 +17,7 @@ var counter = 0;
 var snowFreq;
 var sign;
 var saveCo = 99;
-var version = "Version 2.2.14";
+var version = "Version 2.2.16";
 
 //Setup: Canvas, background, init objects
 function setup() {
