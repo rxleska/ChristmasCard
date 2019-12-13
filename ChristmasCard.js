eed0c3468024773b46259e87f4353c0aed3ed55f
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-12 22:08:09 -0600

    added apple 128x128 icon

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -17,7 +17,7 @@ var counter = 0;
 var snowFreq;
 var sign;
 var saveCo = 99;
-var version = "Version 2.2.9";
+var version = "Version 2.2.10";
 
 //Setup: Canvas, background, init objects
 function setup() {
