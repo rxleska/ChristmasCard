d33b53ef3a65fd377233fccfde64e172b7eb653b
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-12-12 20:54:05 -0600

    new fav icons

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -17,7 +17,7 @@ var counter = 0;
 var snowFreq;
 var sign;
 var saveCo = 99;
-var version = "Version 2.2.5";
+var version = "Version 2.2.6";
 
 //Setup: Canvas, background, init objects
 function setup() {
