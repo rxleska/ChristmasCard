38cfe037f1a209b679cf607236f5d3dfa0eef37a
Ryan Xavier Leska <ryan@leskafamily.com>
2019-12-13 07:48:49 -0600

    changed icon to 512 and removed its link from index

--- ChristmasCard.js
+++ ChristmasCard.js
@@ -17,7 +17,7 @@ var counter = 0;
 var snowFreq;
 var sign;
 var saveCo = 99;
-var version = "Version 2.2.12";
+var version = "Version 2.2.13";
 
 //Setup: Canvas, background, init objects
 function setup() {
